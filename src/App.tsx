1d5736dc33394bebc9d85b5aa86c6c17703781e3
AnaCatanaR <anacatana.ac@gmail.com>
2023-02-01 22:57:40 +0100

    Add early version of keyboard container

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
-import Actions from "./components/Actions/Actions";
 import Container from "./components/Container/Container";
-import Phone from "./components/Phone/Phone";
-import InfoMessage from "./components/InfoMessage/InfoMessage";
 
 const App = (): JSX.Element => {
-  return (
-    <>
-      <Container />
-      <Phone />
-      <InfoMessage />
-      <Actions />
-    </>
-  );
+  return <Container />;
 };
 
 export default App;
