760e783345be64bd6c915280def87b4f64c933fd
AnaCatanaR <anacatana.ac@gmail.com>
2023-02-03 02:31:54 +0100

    Add Keyboard component

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,21 @@ import Actions from "./components/Actions/Actions";
 import Info from "./components/Info/Info";
 import Keyboard from "./components/Keyboard/Keyboard";
 import "./App.css";
+import { useState } from "react";
 
 const App = (): JSX.Element => {
+  let [numberDisplay, setNumberDisplay] = useState("609272100");
+  const numberToAdd = (keyNumber: string) => {
+    setNumberDisplay((numberDisplay = numberDisplay.concat(keyNumber)));
+  };
+
   return (
     <>
       <div className="container">
         <Info />
         <main className="phone">
           <Keyboard />
-          <Actions />
+          <Actions numberDisplay={numberDisplay} />
         </main>
       </div>
     </>
--- src/components/Key/Key.tsx
+++ src/components/Key/Key.tsx
@@ -1,11 +1,12 @@
 import "./Key.css";
 
-const Key = (): JSX.Element => {
-  return (
-    <li>
-      <button className="key"></button>
-    </li>
-  );
+interface KeyProps {
+  keyText: string;
+  className: string;
+}
+
+const Key = ({ keyText, className }: KeyProps): JSX.Element => {
+  return <button className={`key ${className}`}>{keyText}</button>;
 };
 
 export default Key;
